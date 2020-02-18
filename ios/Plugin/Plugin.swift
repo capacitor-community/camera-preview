80c0331d4aa4fee2f9ada6b01c74d100dd84a496
Ariel Hernandez Musa <ahernandez@technisys.com>
2020-02-18 14:20:59 -0300

    Fix camera rotation Close #12

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -9,17 +9,40 @@ import AVFoundation
 public class CameraPreview: CAPPlugin {
 
     var previewView:UIView!
+    var cameraPosition = String()
     let cameraController = CameraController()
+    
+    @objc func rotated() {
+        self.previewView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
+        self.cameraController.previewLayer?.frame = self.previewView.frame
+        
+        if UIDevice.current.orientation.isLandscape {
+            
+            if (UIDevice.current.orientation == UIDeviceOrientation.landscapeLeft) {
+                self.cameraController.previewLayer?.connection?.videoOrientation = .landscapeRight
+            }
+            
+            if (UIDevice.current.orientation == UIDeviceOrientation.landscapeRight) {
+                self.cameraController.previewLayer?.connection?.videoOrientation = .landscapeLeft
+            }
+        }
+
+        if UIDevice.current.orientation.isPortrait {
+            self.cameraController.previewLayer?.connection?.videoOrientation = .portrait
+        }
+    }
 
     @objc func start(_ call: CAPPluginCall) {
         //        let value = call.getString("value") ?? ""
-        let cameraPosition = call.getString("position") ?? "rear"
+        self.cameraPosition = call.getString("position") ?? "rear"
         
+        NotificationCenter.default.addObserver(self, selector: #selector(CameraPreview.rotated), name: UIDevice.orientationDidChangeNotification, object: nil)
+
         DispatchQueue.main.async {
             if (self.cameraController.captureSession?.isRunning ?? false) {
                 call.reject("camera already started")
             } else {
-                self.cameraController.prepare(cameraPosition: cameraPosition){error in
+                self.cameraController.prepare(cameraPosition: self.cameraPosition){error in
                     if let error = error {
                         print(error)
                         call.reject(error.localizedDescription)
