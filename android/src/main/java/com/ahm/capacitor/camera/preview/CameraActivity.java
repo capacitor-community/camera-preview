a81261b54715d4eeec39987fd14cadd0711456ef
Rafael Gomes <rafael.gomes@madsolutions.com.br>
2020-12-06 20:41:51 -0300

    Issue 40

--- android/src/main/java/com/ahm/capacitor/camera/preview/CameraActivity.java
+++ android/src/main/java/com/ahm/capacitor/camera/preview/CameraActivity.java
@@ -655,7 +655,30 @@ public class CameraActivity extends Fragment {
             params.setJpegQuality(quality);
           }
 
-          params.setRotation(mPreview.getDisplayOrientation());
+          if(cameraCurrentlyLocked == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            Activity activity = getActivity();
+            int rotation = activity.getWindowManager().getDefaultDisplay().getRotation();
+            int degrees = 0;
+            switch (rotation) {
+              case Surface.ROTATION_0:
+                degrees = 90;
+                break;
+              case Surface.ROTATION_90:
+                degrees = 180;
+                break;
+              case Surface.ROTATION_180:
+                degrees = 270;
+                break;
+              case Surface.ROTATION_270:
+                degrees = 0;
+                break;
+            }
+            params.setRotation(degrees);
+          } else {
+            params.setRotation(mPreview.getDisplayOrientation());
+          }
+
+          // params.setRotation(mPreview.getDisplayOrientation());
 
           mCamera.setParameters(params);
           mCamera.takePicture(shutterCallback, null, jpegPictureCallback);
