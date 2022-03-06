e4424205d945371d94588f933bd5cffcd31f2944
Peter Bowyer <peter@mapledesign.co.uk>
2022-03-06 15:24:18 +0000

    Apply prettier

--- src/web.ts
+++ src/web.ts
@@ -1,15 +1,14 @@
-import { WebPlugin } from "@capacitor/core";
-import { 
-  CameraPreviewOptions, 
-  CameraPreviewPictureOptions, 
-  CameraPreviewPlugin, 
-  CameraPreviewFlashMode, 
+import { WebPlugin } from '@capacitor/core';
+import {
+  CameraPreviewOptions,
+  CameraPreviewPictureOptions,
+  CameraPreviewPlugin,
+  CameraPreviewFlashMode,
   CameraSampleOptions,
-  CameraOpacityOptions  
-} from "./definitions";
+  CameraOpacityOptions,
+} from './definitions';
 
 export class CameraPreviewWeb extends WebPlugin implements CameraPreviewPlugin {
-
   /**
    *  track which camera is used based on start options
    *  used in capture
@@ -18,38 +17,37 @@ export class CameraPreviewWeb extends WebPlugin implements CameraPreviewPlugin {
 
   constructor() {
     super({
-      name: "CameraPreview",
-      platforms: ["web"],
+      name: 'CameraPreview',
+      platforms: ['web'],
     });
   }
 
   async start(options: CameraPreviewOptions): Promise<{}> {
-    return new Promise(async(resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
+      await navigator.mediaDevices
+        .getUserMedia({
+          audio: !options.disableAudio,
+          video: true,
+        })
+        .then((stream: MediaStream) => {
+          // Stop any existing stream so we can request media with different constraints based on user input
+          stream.getTracks().forEach((track) => track.stop());
+        })
+        .catch((error) => {
+          reject(error);
+        });
 
-      await navigator.mediaDevices.getUserMedia({
-        audio:!options.disableAudio,  
-        video:true}
-      ).then((stream: MediaStream) => {
-        // Stop any existing stream so we can request media with different constraints based on user input
-        stream.getTracks().forEach((track) => track.stop());
-      }).catch(error => {
-        reject(error);
-      });
-
-      const video = document.getElementById("video");
+      const video = document.getElementById('video');
       const parent = document.getElementById(options.parent);
 
       if (!video) {
-        const videoElement = document.createElement("video");
-        videoElement.id = "video";
-        videoElement.setAttribute("class", options.className || "");
+        const videoElement = document.createElement('video');
+        videoElement.id = 'video';
+        videoElement.setAttribute('class', options.className || '');
 
         // Don't flip video feed if camera is rear facing
-        if(options.position !== 'rear'){
-          videoElement.setAttribute(
-            "style",
-            "-webkit-transform: scaleX(-1); transform: scaleX(-1);"
-          );
+        if (options.position !== 'rear') {
+          videoElement.setAttribute('style', '-webkit-transform: scaleX(-1); transform: scaleX(-1);');
         }
 
         const userAgent = navigator.userAgent.toLowerCase();
@@ -91,13 +89,13 @@ export class CameraPreviewWeb extends WebPlugin implements CameraPreviewPlugin {
           );
         }
       } else {
-        reject({ message: "camera already started" });
+        reject({ message: 'camera already started' });
       }
     });
   }
 
   async stop(): Promise<any> {
-    const video = <HTMLVideoElement>document.getElementById("video");
+    const video = <HTMLVideoElement>document.getElementById('video');
     if (video) {
       video.pause();
 
@@ -114,25 +112,23 @@ export class CameraPreviewWeb extends WebPlugin implements CameraPreviewPlugin {
 
   async capture(_options: CameraPreviewPictureOptions): Promise<any> {
     return new Promise((resolve, _) => {
-      const video = <HTMLVideoElement>document.getElementById("video");
-      const canvas = document.createElement("canvas");
+      const video = <HTMLVideoElement>document.getElementById('video');
+      const canvas = document.createElement('canvas');
 
       // video.width = video.offsetWidth;
 
-      const context = canvas.getContext("2d");
+      const context = canvas.getContext('2d');
       canvas.width = video.videoWidth;
       canvas.height = video.videoHeight;
 
       // flip horizontally back camera isn't used
-      if(!this.isBackCamera){
+      if (!this.isBackCamera) {
         context.translate(video.videoWidth, 0);
         context.scale(-1, 1);
       }
       context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
       resolve({
-        value: canvas
-          .toDataURL("image/png")
-          .replace("data:image/png;base64,", ""),
+        value: canvas.toDataURL('image/png').replace('data:image/png;base64,', ''),
       });
     });
   }
@@ -142,7 +138,7 @@ export class CameraPreviewWeb extends WebPlugin implements CameraPreviewPlugin {
   }
 
   async getSupportedFlashModes(): Promise<{
-    result: CameraPreviewFlashMode[]
+    result: CameraPreviewFlashMode[];
   }> {
     throw new Error('getSupportedFlashModes not supported under the web platform');
   }
@@ -154,19 +150,18 @@ export class CameraPreviewWeb extends WebPlugin implements CameraPreviewPlugin {
   async flip(): Promise<void> {
     throw new Error('flip not supported under the web platform');
   }
-  
+
   async setOpacity(_options: CameraOpacityOptions): Promise<any> {
-    const video = <HTMLVideoElement>document.getElementById("video");
+    const video = <HTMLVideoElement>document.getElementById('video');
     if (!!video && !!_options['opacity']) {
-	   video.style.setProperty("opacity", _options['opacity'].toString());
+      video.style.setProperty('opacity', _options['opacity'].toString());
     }
   }
-  
 }
 
 const CameraPreview = new CameraPreviewWeb();
 
 export { CameraPreview };
 
-import { registerWebPlugin } from "@capacitor/core";
+import { registerWebPlugin } from '@capacitor/core';
 registerWebPlugin(CameraPreview);
