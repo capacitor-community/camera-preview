8480213efb3e52beee64771169685d214d257137
Alex Ryltsov <ryltsov.alex@gmail.com>
2022-07-20 19:11:18 +0300

    bugfix: 93 Preview not resized and displayed correctly when orientation changes (WIP)

--- android/src/main/java/com/ahm/capacitor/camera/preview/CameraActivity.java
+++ android/src/main/java/com/ahm/capacitor/camera/preview/CameraActivity.java
@@ -390,6 +390,40 @@ public class CameraActivity extends Fragment {
         }
     }
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        final FrameLayout frameContainerLayout = (FrameLayout) view.findViewById(
+                 getResources().getIdentifier("frame_container", "id", appResourcesPackage)
+        );
+
+        final int previousOrientation = frameContainerLayout.getHeight() > frameContainerLayout.getWidth() ? Configuration.ORIENTATION_PORTRAIT : Configuration.ORIENTATION_LANDSCAPE;
+        // Checks if the orientation of the screen has changed
+        if (newConfig.orientation != previousOrientation) {
+
+            final RelativeLayout frameCamContainerLayout = (RelativeLayout) view.findViewById(
+                    getResources().getIdentifier("frame_camera_cont", "id", appResourcesPackage)
+            );
+
+            frameContainerLayout.getLayoutParams().width = frameCamContainerLayout.getHeight();
+            frameContainerLayout.getLayoutParams().height = frameCamContainerLayout.getWidth();
+
+            frameCamContainerLayout.getLayoutParams().width = frameCamContainerLayout.getHeight();
+            frameCamContainerLayout.getLayoutParams().height = frameCamContainerLayout.getWidth();
+
+            frameContainerLayout.invalidate();
+            frameContainerLayout.requestLayout();
+
+            frameCamContainerLayout.forceLayout();
+
+            mPreview.setCameraDisplayOrientation();
+
+        }
+
+    }
+
+
     public Camera getCamera() {
         return mCamera;
     }
