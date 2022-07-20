1ca92265b3cecff07d86f3c0d227d6d75d5feb67
Alex Ryltsov <ryltsov.alex@gmail.com>
2022-07-20 19:30:27 +0300

    bugfix: 93 Preview not resized and displayed correctly when orientation changes (WIP)

--- android/src/main/java/com/ahm/capacitor/camera/preview/CameraActivity.java
+++ android/src/main/java/com/ahm/capacitor/camera/preview/CameraActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
 import android.graphics.BitmapFactory;
