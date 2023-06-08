f23ce23229d4267e525666cac30c214b73c73126
jcesarmobile <jcesarmobile@gmail.com>
2023-06-08 16:47:39 +0200

    chore: Update demo app to Capacitor 4 (#285)

--- demo/ios/App/App/AppDelegate.swift
+++ demo/ios/App/App/AppDelegate.swift
@@ -47,18 +47,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     return CAPBridge.handleContinueActivity(userActivity, restorationHandler)
   }
 
-  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    super.touchesBegan(touches, with: event)
 
-    let statusBarRect = UIApplication.shared.statusBarFrame
-    guard let touchPoint = event?.allTouches?.first?.location(in: self.window) else { return }
 
-    if statusBarRect.contains(touchPoint) {
-      NotificationCenter.default.post(CAPBridge.statusBarTappedNotification)
-    }
-  }
-
-  #if USE_PUSH
 
   func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
     NotificationCenter.default.post(name: Notification.Name(CAPNotifications.DidRegisterForRemoteNotificationsWithDeviceToken.name()), object: deviceToken)
@@ -68,7 +58,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     NotificationCenter.default.post(name: Notification.Name(CAPNotifications.DidFailToRegisterForRemoteNotificationsWithError.name()), object: error)
   }
 
-#endif
+
 
 }
+
 
