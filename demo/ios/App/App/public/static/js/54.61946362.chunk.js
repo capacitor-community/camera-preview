8966f05a59125a44fc5cae95e7b2bda3efdfbafa
jcesarmobile <jcesarmobile@gmail.com>
2023-06-09 09:23:31 +0200

    chore: Update demo app to Capacitor 5

---
+++ demo/ios/App/App/public/static/js/54.61946362.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpdemo=this.webpackJsonpdemo||[]).push([[54],{138:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return i}));var o=e(12),i=function(){var n=window;n.addEventListener("statusTap",(function(){Object(o.h)((function(){var t=n.innerWidth,e=n.innerHeight,i=document.elementFromPoint(t/2,e/2);if(i){var c=i.closest("ion-content");c&&c.componentOnReady().then((function(){Object(o.f)((function(){return c.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=54.61946362.chunk.js.map
